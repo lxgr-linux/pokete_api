adbb5991629a26729409dbb38a38980dc62a07e2
lxgr-linux <lxgr@protonmail.com>
2022-01-10 12:30:57 +0100

    added messaged to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+echo ":: Downloading..."
 for file in poketes attacks
 do
+	echo " -> $file.py"
 	curl https://raw.githubusercontent.com/lxgr-linux/pokete/master/pokete_data/${file}.py > ./${file}.py
 done
+echo ":: Stating..."
 go run poke_api.go
