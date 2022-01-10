f8593280f930fad4c335cb2e056c10fe2f4493d4
lxgr-linux <lxgr@protonmail.com>
2022-01-10 18:11:38 +0100

    added functions to just show certain attributes

--- poke_api.go
+++ poke_api.go
@@ -16,39 +16,40 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
  
 func handleRequests() {
-    myRouter := mx.NewRouter().StrictSlash(true)
+    myRouter := mux.NewRouter().StrictSlash(true)
     myRouter.HandleFunc("/", homePage)
     myRouter.HandleFunc("/poketes", returnAllPokes)
     myRouter.HandleFunc("/attacks", returnAllAttacks)
     myRouter.HandleFunc("/poketes/{name}", returnSinglePoke)
     myRouter.HandleFunc("/attacks/{name}", returnSingleAttack)
-    log.Fatal(http.ListenAndServe(":8000", nil))
+    log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
 func returnSinglePoke(w http.ResponseWriter, r *http.Request){
-    returnAllJson(w, r, "pokes", mux.Vars(r)["name"])
+    returnAllJson(w, "pokes", mux.Vars(r)["name"])
 }
 
 func returnSingleAttack(w http.ResponseWriter, r *http.Request){
+    returnAllJson(w, "attacks", mux.Vars(r)["name"])
 }
 
 func returnAllPokes(w http.ResponseWriter, r *http.Request){
-    returnAllJson(w, r, "pokes", "")
+    returnAllJson(w, "pokes", "")
 }
 
 func returnAllAttacks(w http.ResponseWriter, r *http.Request){
-    returnAllJson(w, r, "attacks", "")
+    returnAllJson(w, "attacks", "")
 }
 
 func returnAllJson(w http.ResponseWriter, name string, key string) {
-    fmt.Println("Endpoint Hit: returnAllJson", name)
-    pokes, _ := exec.Command("./get_json.py", name).Output()
+    fmt.Println("Endpoint Hit: returnAllJson", name, key)
+    data, _ := exec.Command("./get_json.py", name).Output()
     if key == "" {
-        fmt.Fprintf(w, "%s", pokes)
+        fmt.Fprintf(w, "%s", data)
     } else {
 	var result map[string]interface{}
-	json.Unmarshal([]byte(pokes), &result)
-	res, err := json.Marshal(result[key])
+	json.Unmarshal([]byte(data), &result)
+	res, _ := json.Marshal(result[key])
 	fmt.Fprintf(w, "%s", res)
     }
 }
