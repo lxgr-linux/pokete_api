564df222f916c586807ad970c53e91a184a2503b
lxgr-linux <lxgr@protonmail.com>
2022-01-10 12:27:37 +0100

    added attacks

--- get_json.py
+++ get_json.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 from poketes import pokes
+from attacks import attacks
 
 del pokes["__fallback__"]
 
-print(json.dumps(pokes))
+match sys.argv[1]:
+    case "pokes":
+        print(json.dumps(pokes))
+    case "attacks":
+        print(json.dumps(attacks))
+    case _:
+        print({})
--- poke_api.go
+++ poke_api.go
@@ -15,12 +15,21 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func handleRequests() {
     http.HandleFunc("/", homePage)
     http.HandleFunc("/poketes", returnAllPokes)
+    http.HandleFunc("/attacks", returnAllAttacks)
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
- 
-func returnAllPokes(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Endpoint Hit: returnAllPokes")
-    pokes, _ := exec.Command("./get_json.py").Output()
+
+func returnAllPokes(w http.ResponseWriter,r *http.Request){
+    returnAllJson(w, "pokes")
+}
+
+func returnAllAttacks(w http.ResponseWriter,r *http.Request){
+    returnAllJson(w, "attacks")
+}
+
+func returnAllJson(w http.ResponseWriter, name string) {
+    fmt.Println("Endpoint Hit: returnAllJson", name)
+    pokes, _ := exec.Command("./get_json.py", name).Output()
     fmt.Fprintf(w, "%s", pokes)
 }
  
