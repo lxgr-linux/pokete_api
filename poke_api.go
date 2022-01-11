a96b60347e9315d22db1f9717ebf6caff05acb94
lxgr-linux <lxgr@protonmail.com>
2022-01-12 00:25:58 +0100

    linting

--- poke_api.go
+++ poke_api.go
@@ -1,71 +1,71 @@
 package main
- 
-import (
-    "fmt"
-    "log"
-    "net/http"
-    "os/exec"
-    "encoding/json"
 
-    "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/gorilla/mux"
 )
 
 func itemInList(item string, list []string) bool {
-    for _, i := range list {
-	if i == item {
-	    return true
+	for _, i := range list {
+		if i == item {
+			return true
+		}
 	}
-    }
-    return false
+	return false
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to the Pokete-API!")
-    log.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Welcome to the Pokete-API!")
+	log.Println("Endpoint Hit: homePage")
 }
- 
+
 func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/{cat}", returnJson)
-    myRouter.HandleFunc("/{cat}/{name}", returnJson)
-    log.Fatal(http.ListenAndServe(":8000", myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/{cat}", returnJSON)
+	myRouter.HandleFunc("/{cat}/{name}", returnJSON)
+	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
-func handleNotFound(w http.ResponseWriter){
-    w.WriteHeader(http.StatusNotFound)
-    fmt.Fprintf(w, "Error 404 - Page not found") 
+func handleNotFound(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Error 404 - Page not found")
 }
 
-func returnJson(w http.ResponseWriter, r *http.Request) {
-    cat, _ := mux.Vars(r)["cat"]
-    if ! itemInList(cat, []string{"types", "poketes", "attacks"}) {
-	handleNotFound(w)
-	return
-    }
-    log.Println("Endpoint Hit: returnJson", cat)
-    data, err := exec.Command("./get_json.py", cat).Output()
-    if err != nil {
-	log.Printf("Error in get_json: %s", err)
-	handleNotFound(w)
-	return
-    }
-    key, exists := mux.Vars(r)["name"] 
-    if ! exists {
-        fmt.Fprintf(w, "%s", data)
-    } else {
-	var rawData map[string]interface{}
-	json.Unmarshal([]byte(data), &rawData)
-	item, item_exists := rawData[key]
-	if ! item_exists {
-	    handleNotFound(w)
-	    return
+func returnJSON(w http.ResponseWriter, r *http.Request) {
+	cat, _ := mux.Vars(r)["cat"]
+	if !itemInList(cat, []string{"types", "poketes", "attacks"}) {
+		handleNotFound(w)
+		return
 	}
-	res, _ := json.Marshal(item)
-	fmt.Fprintf(w, "%s", res)
-    }
+	log.Println("Endpoint Hit: returnJson", cat)
+	data, err := exec.Command("./get_json.py", cat).Output()
+	if err != nil {
+		log.Printf("Error in get_json: %s", err)
+		handleNotFound(w)
+		return
+	}
+	key, exists := mux.Vars(r)["name"]
+	if !exists {
+		fmt.Fprintf(w, "%s", data)
+	} else {
+		var rawData map[string]interface{}
+		json.Unmarshal([]byte(data), &rawData)
+		item, itemExists := rawData[key]
+		if !itemExists {
+			handleNotFound(w)
+			return
+		}
+		res, _ := json.Marshal(item)
+		fmt.Fprintf(w, "%s", res)
+	}
 }
- 
+
 func main() {
-    handleRequests()
+	handleRequests()
 }
