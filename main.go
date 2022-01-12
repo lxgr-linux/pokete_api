f6fae402ff2c84b1d366bbd72f6d6d6d5dfd251d
lxgr-linux <lxgr@protonmail.com>
2022-01-12 11:27:19 +0100

    added main.go to replace run.sh

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/lxgr-linux/pokete_api/server"
 )
 
+func showHelp() {
+	fmt.Printf(`Downloads data and starts server
+Usage: %s [ARG1] [ARG2] ...
+Options:
+	--no-download    : Does not download the data
+	--update         : Just downloads the data
+	--port <port>    : The port the server will run on, default is 8000
+	--help           : Shows this dialog`, os.Args[0])
+}
+
 func main() {
-	server.NewServer("8000").HandleRequests()
+	skip := false
+	port := "8000"
+	doDownload := true
+	startServer := true
+
+	for idx, i := range os.Args[1:] {
+		if skip {
+			skip = false
+		} else {
+			switch i {
+			case "--help":
+				showHelp()
+				os.Exit(0)
+			case "--no-download":
+				doDownload = false
+			case "--update":
+				startServer = false
+			case "--port":
+				port = os.Args[idx+2]
+				skip = true
+			default:
+				showHelp()
+				os.Exit(1)
+			}
+		}
+	}
+	if doDownload {
+		cmd := exec.Command("./download.sh")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+	if startServer {
+		fmt.Println(":: Starting...")
+		server.NewServer(port).HandleRequests()
+	}
 }
