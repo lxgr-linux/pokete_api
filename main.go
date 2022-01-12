fa8aaccbb2833d26e5d646b7b628629c282105b0
lxgr-linux <lxgr@protonmail.com>
2022-01-12 13:07:58 +0100

    added downloading to main.go

--- main.go
+++ main.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
-	"os/exec"
 
 	"github.com/lxgr-linux/pokete_api/server"
 )
+
+func download() error {
+	for _, i := range []string{"poketes", "attacks", "types"} {
+		log.Println("Downloading", i)
+		res, err := http.Get("https://raw.githubusercontent.com/lxgr-linux/pokete/master/pokete_data/" + i + ".py")
+		defer res.Body.Close()
+		if err != nil {
+			return err
+		}
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return err
+		}
+		file, err := os.Create("./pokete_" + i + ".py")
+		defer file.Close()
+		if err != nil {
+			return err
+		}
+		_, err = io.WriteString(file, string(body))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func showHelp() {
 	fmt.Printf(`Downloads data and starts server
@@ -46,10 +74,10 @@ func main() {
 		}
 	}
 	if doDownload {
-		cmd := exec.Command("./download.sh")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Run()
+		err := download()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	if startServer {
 		fmt.Println(":: Starting...")
